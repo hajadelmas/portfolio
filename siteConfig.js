6cd013ef2d54eb8967615c7b1b193aede3fcf556
Loic Delmas <loichdelmas@gmail.com>
2020-11-02 18:38:04 +0100

    icons and title

--- siteConfig.js
+++ siteConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: 'PORTFOLIO by Studio Haja',
-  shortName: 'PORTFOLIO by Studio Haja',
+  name: 'Studio Haja',
+  shortName: 'Studio Haja',
   description: 'Bienvenue sur le portfolio du Studio Haja. Vous pouvez consulter mes réalisations et me contacter pour travailler ensemble.',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  url: 'https://studio-haja.fr',
   prefix: '/',
   author: 'Loic Haja Delmas',
   twitter: 'Loic Haja Delmas'
